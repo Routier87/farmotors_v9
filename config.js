bb26e4a26f30af7bad0efabeee347dfa0f7e7ef2
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-08 01:32:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const API_URL = "https://farmotors-v9.onrender.com";
 const DISCORD_CLIENT_ID = "1479893449980514464";
-const DISCORD_REDIRECT_URI = "REMPLACE_PAR_TON_URL_DE_CALLBACK";
-const DISCORD_OAUTH_URL = "";
+const DISCORD_REDIRECT_URI = "https://discord.com/oauth2/authorize?client_id=1479893449980514464&response_type=code&redirect_uri=https%3A%2F%2Froutier87.github.io%2Ffarmotors_v9%2F&scope=identify+guilds+guilds.members.read";
+const DISCORD_OAUTH_URL = "https://routier87.github.io/farmotors_v9/";
   `https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&scope=identify`;
