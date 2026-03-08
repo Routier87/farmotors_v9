a967f1d529afff20e697968c598b4f4ec43b6ae1
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-08 01:29:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-const API_URL = "https://REMPLACE_PAR_TON_BACKEND_RENDER.onrender.com";
-const DISCORD_CLIENT_ID = "REMPLACE_PAR_TON_CLIENT_ID";
+const API_URL = "https://farmotors-v9.onrender.com";
+const DISCORD_CLIENT_ID = "1479893449980514464";
 const DISCORD_REDIRECT_URI = "REMPLACE_PAR_TON_URL_DE_CALLBACK";
-const DISCORD_OAUTH_URL =
+const DISCORD_OAUTH_URL = "";
   `https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&scope=identify`;
